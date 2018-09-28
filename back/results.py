4e25d34d9706937a2c6776b941e1740893252045
tahir <takhir.baiheryieu@softclub.by>
2018-09-28 17:57:48 +0300

    added 'goals' func to get a score from a game

--- back/results.py
+++ back/results.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-from parser import data
+from parser import countries
 
 
-a = np.random.choice(8, 2, p=[0.25, 0.22, 0.18, 0.15, 0.1, 0.05, 0.04, 0.01])
+def goals():
+    score = np.random.choice(8, 2, p=[0.28, 0.25, 0.15, 0.12,
+                                      0.1, 0.05, 0.04, 0.01])
+    return score
