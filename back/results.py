53fbb9df796f87ca98a3d48a89fecc5115e0f9eb
tahir <takhir.baiheryieu@softclub.by>
2018-09-28 17:59:22 +0300

    added 'chance_to_win' func to calculate winning chances of the teams

--- back/results.py
+++ back/results.py
@@ -7,3 +7,12 @@ def goals():
     score = np.random.choice(8, 2, p=[0.28, 0.25, 0.15, 0.12,
                                       0.1, 0.05, 0.04, 0.01])
     return score
+
+
+def chance_to_win(team_1, team_2):
+    team_1_points = int(countries[team_1]["points"])
+    team_2_points = int(countries[team_2]["points"])
+    sum_points = team_1_points + team_2_points
+    team_1_chance = round(team_1_points/sum_points, 2)
+    team_2_chance = round(team_2_points/sum_points, 2)
+    return team_1_chance, team_2_chance
