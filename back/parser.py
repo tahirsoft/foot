c89e085525b12b474b667980cfa779a2fed4ad30
tahir <takhir.baiheryieu@softclub.by>
2018-09-28 12:16:15 +0300

    changed parser.py module to get a different type of 'data' variable again

--- back/parser.py
+++ back/parser.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 
 
-data = []
+countries = {}
 
 with open('test.html') as file:
     parsed_html = BeautifulSoup(file, 'html.parser')
@@ -10,11 +10,9 @@ links = parsed_html.find_all("td", class_="_flag")
 country_points = parsed_html.find_all("td", class_="_points")
 
 for link, points_tag in zip(links, country_points):
-    country = {}
-    flag, country_name = link.img.attrs["src"], link.img.attrs["alt"]
+    flag, country = link.img.attrs["src"], link.img.attrs["alt"]
     points = points_tag.contents[0]
-    country[country_name] = {"flag": flag, "points": points}
-    data.append(country)
+    countries[country] = {"flag": flag, "points": points}
 
-with open('data.txt','w') as f:
-    f.write(str(data))
+with open('data.txt', 'w') as f:
+    f.write(str(countries))
