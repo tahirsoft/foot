55853c98012401cd6cab9e8d94286c3b950317b7
tahir <takhir.baiheryieu@softclub.by>
2018-09-28 11:55:53 +0300

    changed parser.py module to get a different type of 'data' variable

--- back/parser.py
+++ back/parser.py
@@ -10,9 +10,10 @@ links = parsed_html.find_all("td", class_="_flag")
 country_points = parsed_html.find_all("td", class_="_points")
 
 for link, points_tag in zip(links, country_points):
-    country = link.img.attrs
+    country = {}
+    flag, country_name = link.img.attrs["src"], link.img.attrs["alt"]
     points = points_tag.contents[0]
-    country['points'] = points
+    country[country_name] = {"flag": flag, "points": points}
     data.append(country)
 
 with open('data.txt','w') as f:
