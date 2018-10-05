d1955fbf9ea1b6cc3af65ec969ceed0100103e39
tahir <takhir.baiheryieu@softclub.by>
2018-10-05 17:16:33 +0300

    changed functions arguments

--- app/results.py
+++ app/results.py
@@ -1,12 +1,10 @@
 import numpy as np
+import ast
 
 from random import shuffle
 from itertools import combinations
-from group import countries as data
-from group import grouping, default_teams, groups_names, final_grid
 
-
-table = grouping(default_teams)
+from app.group import groups_names, final_grid, grouping
 
 
 def goals() -> list:
@@ -22,8 +20,12 @@ def penalty_goals() -> list:
 
 
 def chance_to_win(team_1: str, team_2: str) -> tuple:
-    team_1_points = int(data[team_1]["points"])
-    team_2_points = int(data[team_2]["points"])
+    with open('app/data.txt', 'r') as f:
+        countries_data = f.read()
+    countries_data = ast.literal_eval(countries_data)
+
+    team_1_points = int(countries_data[team_1]["points"])
+    team_2_points = int(countries_data[team_2]["points"])
     sum_points = team_1_points + team_2_points
     team_1_chance = round(team_1_points/sum_points, 2)
     team_2_chance = round(team_2_points/sum_points, 2)
@@ -44,54 +46,56 @@ def game(team_1: str, team_2: str) -> dict:
     return {team_1: final_score[0], team_2: final_score[1]}
 
 
-def games_in_group(group: str) -> list:
-    countries = table[group].keys()
+def games_in_group(countries: list) -> list:
     group_games = combinations(countries, 2)
     return group_games
 
 
 def group_games_results(group: str) -> list:
-    group_games = games_in_group(group)
+    countries = group.keys()
+    group_games = games_in_group(countries)
     games_results = [game(i[0], i[1]) for i in group_games]
     shuffle(games_results)
     return games_results
 
 
-def group_stats(group: str):
-    for result in group_games_results(group):
-        team_1, team_2 = result.keys()
-        table[group][team_1]["games"] += 1
-        table[group][team_2]["games"] += 1
-        table[group][team_1]["gf"] += result[team_1]
-        table[group][team_2]["gf"] += result[team_2]
-        table[group][team_1]["ga"] += result[team_2]
-        table[group][team_2]["ga"] += result[team_1]
-        table[group][team_1]["diff"] += result[team_1] - result[team_2]
-        table[group][team_2]["diff"] += result[team_2] - result[team_1]
+def group_stats(team_list: list) -> dict:
+    table = grouping(team_list)
+    for group in groups_names:
+        for result in group_games_results(table[group]):
+            team_1, team_2 = result.keys()
+            table[group][team_1]["games"] += 1
+            table[group][team_2]["games"] += 1
+            table[group][team_1]["gf"] += result[team_1]
+            table[group][team_2]["gf"] += result[team_2]
+            table[group][team_1]["ga"] += result[team_2]
+            table[group][team_2]["ga"] += result[team_1]
+            table[group][team_1]["diff"] += result[team_1] - result[team_2]
+            table[group][team_2]["diff"] += result[team_2] - result[team_1]
 
-        if result[team_1] > result[team_2]:
-            table[group][team_1]["won"] += 1
-            table[group][team_2]["lost"] += 1
-            table[group][team_1]["points"] += 3
+            if result[team_1] > result[team_2]:
+                table[group][team_1]["won"] += 1
+                table[group][team_2]["lost"] += 1
+                table[group][team_1]["points"] += 3
 
-        elif result[team_2] > result[team_1]:
-            table[group][team_2]["won"] += 1
-            table[group][team_1]["lost"] += 1
-            table[group][team_2]["points"] += 3
+            elif result[team_2] > result[team_1]:
+                table[group][team_2]["won"] += 1
+                table[group][team_1]["lost"] += 1
+                table[group][team_2]["points"] += 3
 
-        else:
-            table[group][team_1]["draw"] += 1
-            table[group][team_2]["draw"] += 1
-            table[group][team_2]["points"] += 1
-            table[group][team_1]["points"] += 1
+            else:
+                table[group][team_1]["draw"] += 1
+                table[group][team_2]["draw"] += 1
+                table[group][team_2]["points"] += 1
+                table[group][team_1]["points"] += 1
+    return table
 
 
-def final_staging() -> list:
+def final_staging(team_list: list) -> tuple:
     mini_table = {}
-
+    table = group_stats(team_list)
     for group_name in groups_names:
         place = 1
-        group_stats(group_name)
         group = table[group_name]
 
         for k, v in sorted(group.items(),
@@ -106,7 +110,7 @@ def final_staging() -> list:
         [[mini_table[i[0]], mini_table[i[1]]] for i in final_grid],
         []
     )
-    return teams
+    return table, teams
 
 
 def final_stage_results(teams_grid: list) -> dict:
@@ -154,8 +158,8 @@ def kick_out(results: dict) -> list:
     return winners
 
 
-def final_stage_games():
-    teams_grid_one_eighth = final_staging()
+def final_stage_games(team_list):
+    table, teams_grid_one_eighth = final_staging(team_list)
     results_one_eighth = final_stage_results(teams_grid_one_eighth)
 
     teams_grid_quaterfinal = kick_out(results_one_eighth)
@@ -168,4 +172,5 @@ def final_stage_games():
     third_place = final_stage_results(third_placed)
     final = final_stage_results(finalists)
 
-    return final, third_place
+    return (table, results_one_eighth, results_quaterfinal,
+            results_semifinal, third_place, final)
